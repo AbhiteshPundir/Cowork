4940193d6565144cf6ef6363fa9f8347f8a928ac
Abhitesh Pundir <103356440+AbhiteshPundir@users.noreply.github.com>
2024-05-24 13:53:03 +0530

    tour.jsx

--- src/components/Tour.jsx
+++ src/components/Tour.jsx
@@ -7,10 +7,10 @@ const Tour = () => {
     <div className='bg-white h-fit w-full flex flex-col items-center gap-5 p-5 lg:my-20'>
         <div className='flex flex-col items-center'>
           <h1 className='text-xs font-bold mb-3'>A VIRTUAL TOUR</h1>
-          <p className='font-bold text-[25px] lg:text-[53px] lg:tracking-wider'>Explore Cowork Through Our</p>
+          <p className='font-bold text-[23px] lg:text-[53px] lg:tracking-wider'>Explore Cowork Through Our</p>
           <div className='flex items-center'>
               <img className='h-9 w-20 lg:h-14 lg:w-40 rounded-[50px] shadow-md shadow-zinc-400/90' src={img} alt="embeded" />
-              <p className='font-bold text-[25px] lg:text-[53px] lg:tracking-wider'>&nbsp;Lens</p>
+              <p className='font-bold text-[23px] lg:text-[53px] lg:tracking-wider'>&nbsp;Lens</p>
           </div>
         </div>
         <p className='text-sm lg:text-[12px] text-center lg:pt-2 leading-6 lg:w-1/2 my-3'>Experience the essence of Cowork before setting foot in our dynamic spaces. Our immersive video tour gives you a sneak peek into the vibrant atmosphere, cutting-edge facilities, and collaborative energy that define the Cowork experience.</p>
